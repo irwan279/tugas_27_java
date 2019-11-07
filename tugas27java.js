add0c57ad7be7e6fb73206f90776415b18dbd461
irwan muhamad Faizal <37823549+irwan279@users.noreply.github.com>
2019-11-07 09:25:04 +0700

    tugas 27 java

---
+++ tugas27java.js
@@ -0,0 +1,12 @@
+function gabung() {
+  var a = "No ";
+  var b = "Rumah Saya "
+  var c = 30;
+  var d = "A";
+  var e = "jalan Pangeran Tirtayasa No";
+  var f = 70;
+
+  hasil = a + b + c;
+  console.log(hasil);
+}
+gabung();
